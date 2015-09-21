5e0ad6ee85620d84ea9a81ae4d824ff2319a6507
Andrew Hingst <andrewhingst@msn.com>
2015-09-21 15:16:21 -0600

    initial code

---
+++ craps.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+
+int main() {
+	char ans; //initialize variables
+	bool done = false;
+	int die1 = 1;
+	int die2 = 1;
+	int roll = die1 + die2;
+	int cash = 50;
+	int bid = 0;
+	bool win = false;
+	int point = 0;
+
+	while (! done) {
+		
